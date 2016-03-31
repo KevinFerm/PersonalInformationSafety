ccfe8af6142ae7e5abcb2289168b60571da484d9
Kevinferm@gmail.com <Kevinferm@gmail.com>
2016-03-31 13:12:03 +0200

    Started on image decoding stuff

--- security.js
+++ security.js
@@ -4,11 +4,11 @@ function pis(id){
         Version: 0.01,
         Author: "Kevin Ferm",
         Created: "March 2016",
-        Updated: "29/3-16"
+        Updated: "31/3-16"
     };
 
     if(id) {
-        //Doesn't work without this for some reason??
+        //Create instance of function
         if(window == this) {
             return new pis(id);
         }
@@ -36,6 +36,12 @@ pis.prototype = {
                     if(this.emailCheck(this.e[fields[i]].value)) {
                         alert("There is an email address contained in your message, are you sure you want to post this? - " + this.e[fields[i]].value);
                     }
+                    if(this.genderCheck(this.e[fields[i]].value)) {
+                        alert("There is an email address contained in your message, are you sure you want to post this? - " + this.e[fields[i]].value);
+                    }
+                    if(this.phoneCheck(this.e[fields[i]].value)) {
+                        alert("There is an email address contained in your message, are you sure you want to post this? - " + this.e[fields[i]].value);
+                    }
                     console.log(this.e[fields[i]].value);
                 } else if(this.e[fields[i]].type == "file") {
                     console.log("FILE TYPE");
@@ -61,7 +67,8 @@ pis.prototype = {
     if(input.files && input.files[0]) {
         var reader = new FileReader();
         reader.onload = function(e) {
-            console.log(e);
+            //console.log(e.target.result);
+            base64Decode(e.target.result);
             return e;
         }
         reader.readAsDataURL(input.files[0]);
@@ -71,5 +78,21 @@ pis.prototype = {
    emailCheck: function(input) {
     var pattern = /^(\w|\-|\_|\.)+\@((\w|\-|\_)+\.)+[a-zA-Z]{2,}$/;
     return pattern.test(input);
+   },
+
+   genderCheck: function(input) {
+    var pattern = /^(fe)?male$/;
+    return pattern.test(input);
+   },
+
+   phoneCheck: function(input) {
+    var pattern = /^([+]47)?((38[{8,9}|0])|(34[{7-9}|0])|(36[6|8|0])|(33[{3-9}|0])|(32[{8,9}]))([\d]{7})$/
    }
 };
+
+function base64Decode(base64) {
+    var data = base64.split(",");
+    var imgtype = data[0].match(/:([^}]*);/)[1];
+    var decoded = atob(data[1]);
+    console.log(decoded);
+}
